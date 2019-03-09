ac333c0390ab4f1e684f45188ab166daccbfb01d
Cody Yantis <cyantis@gmail.com>
2019-03-09 17:18:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,8 @@ def load_library(file_path)
     [emo_hash][get_meaning][key] = value[1]
     [emo_hash][get_emoticon][key] = value[0]
   end
-  return emo_hash
+  binding.pry
+  emo_hash
 end
 
 def get_japanese_emoticon
