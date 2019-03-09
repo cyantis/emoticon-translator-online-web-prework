cb1f640062696b440daf08b2790e6aafd1b9db6c
Cody Yantis <cyantis@gmail.com>
2019-03-09 17:18:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,12 @@ require 'pry'
 
 def load_library(file_path)
   emo = YAML.load_file(file_path)
-  get_meaning = {}
-  get_emoticon = {}
+  emo_hash = {get_meaning = {}, get_emoticon = {}
   emo.each do |key, value|
-    get_meaning[key] = value[1]
-    get_emoticon[key] = value[0]
+    [emo_hash][get_meaning][key] = value[1]
+    [emo_hash][get_emoticon][key] = value[0]
   end
-  return get_meaning, get_emoticon
+  return emo_hash
 end
 
 def get_japanese_emoticon
