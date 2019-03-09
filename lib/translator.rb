b2ceec70eacc9a9307bf7bd0027af65a4cc2be57
Cody Yantis <cyantis@gmail.com>
2019-03-09 17:18:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def load_library(file_path)
   emo = YAML.load_file(file_path)
-  emo_hash = {get_meaning = {}, get_emoticon = {}
+  emo_hash = {get_meaning = {}, get_emoticon = {}}
   emo.each do |key, value|
     [emo_hash][get_meaning][key] = value[1]
     [emo_hash][get_emoticon][key] = value[0]
