b1d36811d1042617f7a44b628f700067926f9032
Cody Yantis <cyantis@gmail.com>
2019-03-09 17:08:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,10 @@ require 'pry'
 
 def load_library(file_path)
   emo = YAML.load_file(file_path)
-  binding.pry
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
